4bad6b9ed4f83db98c45d359d60b48d2ed129ffc
castrigno22 <castrigno.nicolo@iisgalvanimi.edu.it>
2022-10-27 14:10:02 +0000

    punto2-3

--- birre/src/app/app-routing.module.ts
+++ birre/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemBirreComponent } from './item-birre/item-birre.component';
+import { SearchBirreComponent } from './search-birre/search-birre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component:  SearchBirreComponent},
+  { path: 'item/:id', component: ItemBirreComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- birre/src/app/app.module.ts
+++ birre/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBirreComponent } from './search-birre/search-birre.component';
+import { ItemBirreComponent } from './item-birre/item-birre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBirreComponent,
+    ItemBirreComponent
   ],
   imports: [
     BrowserModule,
